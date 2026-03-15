7ce6860283c496d8147351406d8adc74bacb1abb
Homin Lee <homin.lge@gmail.com>
2026-03-15 22:23:01 +0900

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,6 @@ from utils import fix_markdown_symbol_issue
 MODEL_OPTIONS = [
     "gemini-flash-lite-latest",
     "gemini-3-flash-preview",
-    "gemini-3-pro-preview",
     "gemini-3.1-pro-preview",
     "gemini-2.5-flash-lite",
     "gemini-2.5-flash",
